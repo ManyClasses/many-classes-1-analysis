5a525e0aa23f42d83ac3750a6e95c4a34a45d4b0
Josh de Leeuw <josh.deleeuw@gmail.com>
2019-03-19 12:24:55 -0400

    model runs!

--- fake-data.R
+++ fake-data.R
@@ -1,4 +1,6 @@
-n.classes <- 20
+n.classes <- 40
+metric.factors <- c('class.size', 'proportion.lecture','n.assignments','n.exams','admission.rate')
+nominal.factors <- c('discipline', 'format', 'class.level')
 
 classes <- data.frame(
   id=1:n.classes,
@@ -12,6 +14,12 @@ classes <- data.frame(
   n.exams=2+sample(c(0,2),n.classes, prob = c(0.8, 0.2), replace=T),
   admission.rate=rbeta(n.classes,5,2)
 )
+
+# convert to numeric factors
+classes$incentive <- as.numeric(factor(classes$incentive, levels=c("NoIncentive", "Incentive")))
+classes$discipline <- as.numeric(factor(classes$discipline, levels=c("STEM", "Non-STEM")))
+classes$format <- as.numeric(factor(classes$format, levels=c("Classroom", "Online", "Hybrid")))
+classes$class.level <- as.numeric(factor(classes$class.level, levels=c("Intro", "Intermediate", "Advanced")))
 
 student.data <- NA
 for(c in 1:n.classes){
@@ -29,13 +37,37 @@ for(c in 1:n.classes){
 }
 
 # summaries for JAGS
+delta.z <- student.data$delta.z
 n.students <- nrow(student.data)
+class.index <- student.data$class.id
+class.condition <- classes$incentive
 n.metric.moderators <- 5
 n.nominal.moderators <- 3
 n.levels.nominal.moderator <- c(2,3,3)
+n.conditions <- 2
+y.metric <- array(data=t(as.matrix(classes)[,metric.factors]), dim=c(n.metric.moderators, n.classes))
+y.nominal <- array(data=t(as.matrix(classes)[,nominal.factors]), dim=c(n.nominal.moderators, n.classes))
 
-class.index <- student.data$class.id
-class.condition <- as.numeric(factor(classes$incentive, levels=c("NoIncentive", "Incentive")))
+# packaging data for JAGS
+jags.data <- list(
+  delta.z=delta.z,
+  n.students=n.students,
+  class.index=class.index,
+  class.condition=class.condition,
+  n.metric.moderators=n.metric.moderators,
+  n.nominal.moderators=n.nominal.moderators,
+  n.conditions=n.conditions,
+  n.classes=n.classes,
+  n.levels.nominal.moderator=n.levels.nominal.moderator,
+  y.metric=y.metric,
+  y.nominal=y.nominal
+)
 
-y.metric <- matrix()
-y.nominal <- matrix()
+# run jags
+library(runjags)
+run.jags('institution-and-classroom-level-jags-model.txt', 
+         data=jags.data, 
+         n.chains=3,
+         sample = 1000,
+         burnin = 1000,
+         monitor = c('mu.class', 'sigma.class','beta.intercept', 'beta.metric', 'beta.nominal', 'sigma.mode', 'sigma.sd'))
